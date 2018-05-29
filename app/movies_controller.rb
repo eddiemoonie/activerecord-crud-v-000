3626b4a5af8269d050d29e70920d4f612b98990c
Eddie Moon <eddiemoonie@gmail.com>
2018-05-29 19:03:13 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = { title: "Iron Man", release_date: 2008, director: "Jon Favreau" })
+def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 2008, director: "Jon Favreau" })
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
