4b288dbed5c7ae97e2eac4f40684e8bba26efb89
Eddie Moon <eddiemoonie@gmail.com>
2018-05-29 18:56:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,8 +26,8 @@ end
 
 def can_be_created_in_a_block(args = { title: "Iron Man", release_date: 2008 })
   Movie.create do |m|
-    movie.string :title
-    movie.datetime :release_date
+    movie.string = args[:title]
+    movie.datetime = args[:release_date]
   end
 end
 
